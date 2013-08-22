500e7a3b1cbe6d396ec7de7b68954e5d2da2ce8c
Bruno Amaral <mail@brunoamaral.eu>
2013-08-23 00:07:42 +0100

    hardcoding style.css

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 	
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!-- findme -->
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="<?php get_stylesheet_directory(); ?>/style.css">
 	<!-- findme -->
 	<?php wp_head(); ?>
   </head>

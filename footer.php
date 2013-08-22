cbc999135638aac0a2a0025311582d4952cc958e
Bruno Amaral <mail@brunoamaral.eu>
2013-08-22 23:42:16 +0100

    first draft of php files

---
+++ footer.php
@@ -0,0 +1,25 @@
+  <footer id="main">
+    <div class="container">
+      <div class="col-9">
+
+        <span class="sprite"><i class="icon-facebook icon-large"></i><a href="#">&nbsp;&nbsp;&nbsp;Facebook</a></span>
+        <span class="sprite"><i class="icon-twitter icon-large"></i><a href="#">&nbsp;&nbsp;&nbsp;Twitter</a></span>  
+        <span class="sprite"><i class="icon-google-plus icon-large"></i><a href="#">&nbsp;&nbsp;&nbsp;Google+</a></span>
+        <span class="sprite"><i class="icon-linkedin icon-large"></i><a href="#">&nbsp;&nbsp;&nbsp;Linkedin</a></span>
+        <span class="sprite"><i class="icon-rss icon-large"></i><a href="#">&nbsp;&nbsp;&nbsp;RSS Feed</a></span>
+      
+      </div>
+      
+      <div class="col-3 pull-right">
+        <p class="muted">&copy; 2013 A Different Perspective</p>
+      </div>
+    </div>
+  </footer>
+  
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.js"></script>
+
+    <!-- Enable responsive features in IE8 with Respond.js (https://github.com/scottjehl/Respond) -->
+    <script src="assets/js/respond.min.js"></script>
---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-GB">
+
+  <head>
+    <title>A Different perspective</title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php get_stylesheet(); ?> 
+
+	<?php wp_head(); ?>
+  </head>
+

f35c01afda215f86e090409bd3ad0cd97639bafb
brunoamaral <mail@brunoamaral.eu>
2013-08-25 14:41:37 +0100

    added custom comment html

--- comments.php
+++ comments.php
@@ -24,16 +24,16 @@
 
 	<?php if ( ! empty( $comments_by_type['comment'] ) ) { ?>
 		<div id="comments" class="">
-			<h3><?php comments_number( __( 'No responses', 'standard' ), __( 'One response', 'standard' ), __( '% responses', 'standard' ) );?> <?php _e( 'to',  'standard' ); ?> <em><?php the_title(); ?></em></h3>
+			<h3><?php comments_number( 'No coments', 'One comment', '% comments' );?> to <em><?php the_title(); ?></em></h3>
 			<ol class="commentlist">
-				<?php wp_list_comments( 'avatar_size=50&callback=standard_custom_comment&type=comment' ); ?>
+				<?php wp_list_comments( 'avatar_size=50&callback=adp_custom_comment&type=comment' ); ?>
 			</ol>    
 			<div class="comment-navigation ">
 				<div class="comment-prev-nav">
-					<?php previous_comments_link( '<i class="icon-chevron-left"></i>' . __( 'Previous Comments', 'standard' ) ); ?>
+					<?php previous_comments_link( '<i class="icon-chevron-left"></i>' . 'Previous Comments' ); ?>
 				</div>
 				<div class="comment-next-nav">
-					<?php next_comments_link( __( 'Next Comments', 'standard' ) . '<i class="icon-chevron-right"></i>'); ?>
+					<?php next_comments_link( 'Next Comments' . '<i class="icon-chevron-right"></i>'); ?>
 				</div>
 			</div>
 		</div><!-- /#comments -->
@@ -41,9 +41,7 @@
 
 	<?php if ( ! empty( $comments_by_type['pings'] ) ) { ?>
 		<div id="pings">
-			<h3>
-				<?php _e( 'Trackbacks and Pingbacks:', 'standard' ); ?>
-			</h3>
+			<h3> Trackbacks and Pingbacks: </h3>
 			<ol class="pinglist">
 				<?php wp_list_comments( 'type=pings&callback=list_pings&per_page=-1' ); ?>
 			</ol>
@@ -54,8 +52,8 @@
 
 	<?php if( comments_open() ) { ?>
 		<div id="no-comments" class="">
-			<p class="title"><?php _e( 'No Comments', 'standard' ); ?></p>
-			<p><?php _e( 'Be the first to start the conversation.', 'standard' ); ?></p>
+			<p class="title">No Comments</p>
+			<p>Be the first to start the conversation.</p>
 		</div><!-- /#no-comments -->
 	<?php } // end if ?>
 	
--- functions.php
+++ functions.php
@@ -165,4 +165,64 @@ function adp_navigation(){
         <?php
 }
 
+// COMMENTS
+
+function adp_custom_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment; ?>
+
+	<li <?php comment_class( 'clearfix' ); ?> id="li-comment-<?php comment_ID(); ?>">
+
+		<div class="comment-container clearfix">
+
+			<?php if ( "comment" == get_comment_type() ) { ?>
+				<div class="avatar-holder">
+					<?php echo get_avatar( get_comment_author_email(), '50' ); ?>
+				</div><!-- /.avatar-holder -->
+			<?php } // end if ?>
+
+			<div class="comment-entry"	id="comment-<?php comment_ID(); ?>">
+
+				<div class="comment-head">
+					<span class="name">
+						<?php if( '' == get_comment_author_url() ) { ?>
+							<?php comment_author(); ?>
+						<?php } else { ?>
+							<a href="<?php comment_author_url(); ?>" target="_blank"><?php comment_author(); ?></a>
+						<?php } // end if/else ?>
+					</span>
+					<?php if ( get_comment_type() == "comment" ) { ?>
+						<span class="date"><a href="<?php echo get_comment_link(); ?>" title="<?php esc_attr_e( 'Permalink', 'standard'); ?>"><?php printf( __( '%1$s at %2$s', '_s' ), get_comment_date( get_option( 'date_format' ) ), get_comment_time( get_option( 'time_format' ) ) ); ?></a></span>
+						<span class="edit"><?php edit_comment_link( __( 'Edit', 'standard' ), '', '' ); ?></span>
+					<?php } // end if ?>
+				</div><!-- /.comment-head -->
+
+				<?php if ( '0' == $comment->comment_approved ) { ?>
+					<span class='unapproved label warning'>
+						Your comment will appear after being approved.
+					</span>
+				<?php } // end if ?>
+
+				<div class="comment-text">
+					<?php comment_text(); ?>
+				</div><!-- /.comment-text -->
+
+				<div class="reply clearfix">
+					<?php
+						comment_reply_link(
+							array_merge(
+								$args,
+								array(
+									'depth' 		=> $depth,
+									'max_depth' 	=> $args['max_depth'],
+									'reply_text' 	=> __( 'Reply', 'standard')
+								)
+							)
+						);
+						?>
+				</div><!-- /.reply -->
+
+			</div><!-- /.comment-entry -->
+		</div><!-- /comment-container -->
+<?php } // end adp_custom_comment
+
 ?>

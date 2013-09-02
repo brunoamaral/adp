dfe0d983cf90f38e7c22c54d709a932cf8d33503
brunoamaral <mail@brunoamaral.eu>
2013-09-02 02:44:05 +0100

    added search form and search results page

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 	<label>
 		<div class="input-group">
 			<span class="sr-only">Search for:</span>
-			<span class="input-group-addon"><i class="icon-search"></i> </span>
+			<span class="input-group-addon"><i class="icon-search"></i></span>
 			<input type="search" class="search-field form-control" placeholder="Search …" value="" name="s" title="Search for:" />
 		</div>
 	</label>

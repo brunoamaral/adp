4eff4e197bd2355e237a0fc35cea78f2ea8e9622
brunoamaral <mail@brunoamaral.eu>
2013-09-04 00:34:46 +0100

    clean up

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?Php get_header(); ?>
 
-
-
         <?php adp_header(); ?>
 
         <?php adp_navigation(); ?>
 
         <?php get_template_part( 'loop', get_post_format() ); ?>
-      
-
 
 <?php get_footer(); ?>

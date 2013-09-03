e1b3d37409ea039cfe3df911fc169c6b4c1f6fe5
brunoamaral <mail@brunoamaral.eu>
2013-09-03 23:33:42 +0100

    minor fix to label

--- searchform.php
+++ searchform.php
@@ -2,10 +2,12 @@
 <a class="trigger_search" href="#"><i class="icon-search"></i>  Search</a>
 
 <form role="search" method="get" class="form-inline search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
+	
 		<div class="input-group">
+			<label>
 			<span class="sr-only">Search for:</span>
 			<input type="search" class="search-field form-control" placeholder="type and press enter to search" value="" name="s" title="Search for:" />
+			</label>
 		</div>
-	</label>
+	
 </form>
